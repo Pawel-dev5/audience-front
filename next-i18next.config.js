5237b79f8286ba933ca9c5172fc16fb2c91622b8
Pawel-dev5 <p.nowecki@gmail.com>
2022-07-11 18:59:21 +0200

    prod test

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
-  i18n: {
-    defaultLocale: "pl",
-    locales: ["en", "pl"],
-  },
+	i18n: {
+		defaultLocale: 'pl',
+		locales: ['en', 'pl'],
+	},
+	images: {
+		domains: ['https://radiant-river-21361.herokuapp.com'],
+		// loader: "imgix",
+		// path: "",
+	},
 };
